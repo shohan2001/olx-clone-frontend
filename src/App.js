d6e563d1bc9576c0c31e9bb1d8d5f00a0ce0c74f
shohan2001 <shohanmondal3@gmail.com>
2022-07-06 01:37:00 +0530

    ex

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
                             />
 
                             <Route
+                                exact
                                 path="/category/:id"
                                 element={<CategoryPage />}
                             />
